7e79aa454dce3e7b669b97b93477f2dbf7caa8f3
Valentin <frezer.2021@mail.ru>
2023-03-13 19:32:28 +0300

    2

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
-file = open('1.txt', 'r', encoding='utf-16')
+from tkinter import *
+from tkinter import filedialog
 
+# def clicked():
+#     file = filedialog.askopenfilename(filetypes = (("Text files","*.txt"),("all files","*.*")))
+#     return file
+# window = Tk()
+# window.title("Добро пожаловать!")
+# window.geometry('350x200')
+
+# btn = Button(window, text="Загрузить файл", command=clicked)
+# btn.grid(column=1, row=0)
+# window.mainloop()
+file = open('1.txt', 'r', encoding='utf-16')
 list_1 = list()
 resultData = list()
 for line in file.readlines():
-    resultData.append(list(line.split('\n')))#[0].split(':')
-
-
+    resultData.append(list(line.split('\n')))  # [0].split(':')
 file.close()
-j=39
-print(len(resultData))
+numA = 0
+numB = 0
+numC = 0
+j = 39
 for i in resultData:
-    print(resultData[j].remove('Длина рабочей подачи:'))
-    # stringA = str(resultData[j])
-    # print(stringA)
-    # numA = int(stringA[17:])
-    #print(numA)
-    print(resultData[j+1])
-    print(resultData[j+2])
-    j +=25 
-    if j>len(resultData): break  
+    stringA = str(resultData[j])
+    stringA = stringA[23:-11]
+    numA += int(stringA)
+
+    stringB = str(resultData[j+1])
+    stringB = stringB[24:-8]
+    numB += int(stringB)
+
+    stringC = str(resultData[j+2])
+    stringC = stringC[28:-9]
+    numC += int(stringC)
+
+    j += 25
+    if j > len(resultData):
+        break
+
+print('Длина рабочей подачи: ', numA)
+print('Длина подачи врезания: ', numB)
+print('Длина быстрых перемещений: ', numC)
+print('итого: ', numA+numB)
+#https://habr.com/ru/company/vdsina/blog/557316/
+#https://dvsemenov.ru/tkinter-primery-i-rukovodstvo-kak-delat-gui-na-python/#i-4
